985145dcaeb638693990c0d4a27a7d12aa9c7f9d
tianshu <doglooksgood@gmail.com>
2020-02-27 02:27:36 +0800

    tweaking

--- joker-theme.el
+++ joker-theme.el
@@ -1,64 +1,48 @@
-;;; joker-theme.el --- A minimal theme for Emacs in terminal
+;;; -*- lexical-binding: t -*-
 
-;; Copyright (C) 2020 Shi Tianshu
-
-;; Author: Shi Tianshu<doglooksgood@gmail.com>
-;; URL: http://github.com/DogLooksGood/joker-theme
-;; Version: 0.1
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://W.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;; A minimal theme for Emacs in terminal.
-
-;;; Code:
-
-(deftheme joker "A minimal theme for Emacs in terminal.")
+(deftheme joker "A simple medium contrast dark theme.")
 
 (custom-theme-set-faces
  'joker
- '(default                        ((t (:foreground "#a8a8a8"))))
+ `(default                        ((t (:foreground "#A8A8A8" :background "#211F23"))))   ;6.9/1
  '(mc/cursor-face                 ((t (:background "#949494" :foreground "black"))))
- '(region                         ((t (:background "#00AF5F" :foreground "black"))))
- '(hl-line                        ((t (:background "black"))))
- '(show-paren-match               ((t (:background "#00D787" :foreground "black"))))
+ '(region                         ((t (:background "#4F0030"))))
+ '(highlight-symbol-face          ((t ())))
+ `(hl-line                        ((t (:background "#272727"))))
+ '(yascroll:thumb-fringe          ((t (:background "#3F3F3F" :foreground "#3F3F3F"))))
+ '(yascroll:thumb-text-area       ((t (:background "#3F3F3F" :foreground "#3F3F3F"))))
+ '(fringe                         ((t (:background nil))))
+ '(window-divider                 ((t (:foreground "#3F3F3F"))))
+ '(show-paren-match               ((t (:underline "#00AF5F"))))
  '(company-tooltip-common         ((t (:foreground "#D7005F"))))
  '(company-tooltip-common-selection ((t (:foreground "black"))))
  '(company-tooltip                ((t (:background "black"))))
  '(company-tooltip-selection      ((t (:background "#D7AF00" :foreground "black"))))
  '(company-scrollbar-bg           ((t (:background "#303030"))))
  '(company-scrollbar-fg           ((t (:background "#4E4E4E"))))
- '(font-lock-comment-face         ((t (:foreground "#00AFAF"))))
- '(font-lock-string-face          ((t (:foreground "#D7005F"))))
- '(font-lock-doc-face             ((t (:foreground "#00AFAF"))))
+ '(font-lock-comment-face         ((t (:foreground "#00BBBB"))))
+ '(font-lock-string-face          ((t (:foreground "#CC2F2F"))))
+ '(font-lock-doc-face             ((t (:foreground "#00BBBB"))))
  '(font-lock-builtin-face         ((t ())))
  '(font-lock-type-face            ((t ())))
  '(font-lock-variable-name-face   ((t ())))
  '(font-lock-keyword-face         ((t ())))
- '(font-lock-constant-face        ((t (:foreground "#D7AF00"))))
- '(font-lock-function-name-face   ((t (:foreground "#00AFD7"))))
- '(m4d-insert-indicator           ((t (:foreground "#D7AF00"))))
+ '(font-lock-constant-face        ((t (:foreground "#CFA300"))))
+ '(font-lock-function-name-face   ((t (:foreground "#00BBBB"))))
+ '(font-lock-warning-face         ((t (:foreground "yellow"))))
+ '(m4d-kmacro-indicator           ((t (:foreground "#CC2F2F"))))
+ '(m4d-insert-indicator           ((t (:foreground "#CFA300"))))
  '(m4d-visual-indicator           ((t (:foreground "#00D787"))))
- '(cider-result-overlay-face      ((t (:background "#000087"))))
- '(mode-line                      ((t ())))
+ '(m4d-motion-indicator           ((t (:foreground "#00AFD7"))))
+ '(cider-result-overlay-face      ((t (:background "black"))))
+ '(mode-line                      ((t (:background "#000000"))))
+ '(mode-line-inactive             ((t (:background "#303030"))))
  '(clojure-character-face         ((t (:inherit font-lock-constant-face))))
- '(highlight                      ((t (:background "#00AFAF" :foreground "black"))))
+ '(highlight                      ((t (:background "#00BBBB" :foreground "black"))))
  '(isearch                        ((t (:background "#00D75F" :foreground "black"))))
- '(isearch-fail                   ((t (:backgronud "#D75FFF" :foreground "black"))))
- '(popup-isearch-match            ((t (:background "#D7AF00" :foreground "black"))))
- '(ivy-highlight-face             ((t (:backgronud "#00AFFF" :foreground "black"))))
+ '(isearch-fail                   ((t (:backgronud "#D75FFF" :foreground "#606060"))))
+ '(popup-isearch-match            ((t (:background "#CFA300" :foreground "black"))))
+ '(ivy-highlight-face             ((t ())))
  '(ivy-yanked-word                ((t (:background "yellow" :foreground "black"))))
  '(ivy-remote                     ((t ())))
  '(vertical-border                ((t (:background nil :foreground "black"))))
@@ -68,24 +52,38 @@
  '(lazy-highlight                 ((t (:foreground "white"))))
  '(magit-diff-file-heading-highlight ((t (:background "#000087"))))
  '(magit-section-highlight           ((t (:background "#000087"))))
- '(swiper-background-match-face-1 ((t (:background "#00D75F" :foreground "black"))))
- '(swiper-background-match-face-2 ((t (:background "#00D75F" :foreground "black"))))
- '(swiper-background-match-face-3 ((t (:background "#00D75F" :foreground "black"))))
- '(swiper-background-match-face-4 ((t (:background "#00D75F" :foreground "black"))))
+ '(magit-diff-removed             ((t (:inherit font-lock-string-face))))
+ '(magit-diff-added               ((t (:inherit font-lock-comment-face))))
+ '(magit-diff-removed-highlight   ((t (:inherit font-lock-string-face :background "#303030"))))
+ '(magit-diff-added-highlight     ((t (:inherit font-lock-comment-face :background "#303030"))))
+ '(magit-diff-highlight           ((t (:background "#303030"))))
+ '(magit-diff-context-highlight   ((t (:background "#303030"))))
+ '(swiper-background-match-face-1 ((t (:inherit hl-line))))
+ '(swiper-background-match-face-2 ((t (:inherit hl-line))))
+ '(swiper-background-match-face-3 ((t (:inherit hl-line))))
+ '(swiper-background-match-face-4 ((t (:inherit hl-line))))
  '(solaire-default-face           ((t (:background "#262626"))))
+ '(compilation-info               ((t (:inherit font-lock-function-name-face))))
+ '(match                          ((t (:inherit font-lock-doc-face))))
  '(swiper-match-face-1            ((t (:foreground "white"))))
  '(swiper-match-face-2            ((t (:foreground "white"))))
  '(swiper-match-face-3            ((t (:foreground "white"))))
  '(swiper-match-face-4            ((t (:foreground "white"))))
- '(ivy-current-match              ((t (:background "#D7AF00" :foreground "black"))))
+ '(ivy-current-match              ((t (:inverse-video t))))
  '(ivy-minibuffer-match-highlight ((t (:foreground "#00D7D7"))))
- '(ivy-minibuffer-match-face-1    ((t (:foreground "#9E9E9E"))))
- '(ivy-minibuffer-match-face-2    ((t (:foreground "#D7005F"))))
- '(ivy-minibuffer-match-face-3    ((t (:foreground "#D7AF00"))))
- '(ivy-minibuffer-match-face-4    ((t (:foreground "#00FFD7"))))
- '(yas-field-highlight-face       ((t (:background "#00D787" :foreground "black")))))
+ '(ivy-minibuffer-match-face-1    ((t ())))
+ '(ivy-minibuffer-match-face-2    ((t ())))
+ '(ivy-minibuffer-match-face-3    ((t ())))
+ '(ivy-minibuffer-match-face-4    ((t ())))
+ '(yas-field-highlight-face       ((t (:background "#350035"))))
+ '(company-template-field         ((t (:inherit yas-field-highlight-face))))
+ '(org-document-title             ((t (:inherit font-lock-string-face))))
+ '(org-code                       ((t (:inherit font-lock-constant-face))))
+ '(line-number-current-line       ((t (:foreground "#D7AF00"))))
+ '(parenthesis                    ((t (:foreground "#606060"))))
+ '(term-color-blue                ((t (:foreground "#3366FF" :background "#3366FF"))))
+ '(popup-tip-face                 ((t (:background "#303030" :foreground "#C5C5C5")))))
 
-;;;###autoload
 (and load-file-name
      (boundp 'custom-theme-load-path)
      (add-to-list 'custom-theme-load-path
@@ -93,5 +91,3 @@
                    (file-name-directory load-file-name))))
 
 (provide-theme 'joker)
-
-;;; joker-theme.el ends here
